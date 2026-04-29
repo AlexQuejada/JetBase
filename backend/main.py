91d06c82a6c71d8d018d4ea9981ce23333daa693
Alex Gabriel Quejada <91655369+AlexQuejada@users.noreply.github.com>
2026-04-29 15:05:10 -0500

    fix enlace back

--- backend/main.py
+++ backend/main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://https://www.flintrex.com"],
+    allow_origins=["http://localhost:3000","https://www.flintrex.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
